3e8cfb52964347bcfde5bd4fb35fed6693a017db
Varner Damasceno <44266193+VarnerDamascenoJr@users.noreply.github.com>
2019-07-11 08:06:06 -0500

    Funções para barras melhoradas.

--- pipe.js
+++ pipe.js
@@ -4,11 +4,26 @@ function Pipe(){
 	this.x = width;
     this.w = 20;
     this.speed = 2;
-	this.show(){
+   this.highlight = false;
+	
+	this.show = function(){
 		fill(255);
+		if (this.highlight) { fill(255, 0, 0);}
 		rect(this.x, 0, 20, this.w, this.top);  // Aqui para a criacao de retangulos superiores 
 		rect(this.x, height - this.bottom,  this.w, this.bottom);// Aqui para a criacao de retangulos inferiores
 
+	}
+
+	this.hits = function(bird){ //verificara se o objeto toca nas colunas
+        if (bird.y < this.top || bird.y > height - this.bottom) {
+          if (bird.x > this.x && bird.x < this.x + this.w) {
+          	this.highlight = true;
+
+          	return true;
+          }
+           }
+           this.highlight = false;
+           return false;
 	}
 
 	this.update = function(){
