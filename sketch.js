7e01021b194a2e7d02171871f359f4a295190a4f
Varner Damasceno <44266193+VarnerDamascenoJr@users.noreply.github.com>
2019-07-10 19:23:13 -0500

    Adição de funções.

--- sketch.js
+++ sketch.js
@@ -1,8 +1,9 @@
 var bird;
-
+var pipes = [];
 function setup() {
   createCanvas(400, 400);
   bird = new Bird();
+  pipes.push(new Pipe());
 }
 
 function draw() {
@@ -11,10 +12,26 @@ function draw() {
  bird.update();
  bird.show();
 
+
+if (frameCount % 100 == 0) {
+	pipes.push(new Pipe()); //a cada intervalo de frames sera adicionado uma nova coluna
+}
+
+
+
+
+ for (var i = pipes.length -1; i>= 0; i--) {
+ 	pipes[i].show();
+ 	pipes[i].update();
+ }
+ if (pipes[i].offcreen()) {
+ 	pipes.splice(i, 1);
+ }
+
 }
 
 function keyPressed(){
 	if (key == ' ') {
-		bird.up();
+		bird.up(); //Cada vez que eu pressionar a barra, entao, o objeto será atualizado
 	}
 }
